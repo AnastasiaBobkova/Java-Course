6ebdf5b15a53df0abda5108dbae81dd0ff894c4a
AnastasiaBobkova <aubobkova@gmail.com>
2025-02-15 14:27:07 +0300

    First task of home assignment 3

---
+++ src/homeassignments/task3/Equation.java
@@ -0,0 +1,11 @@
+package homeassignments.task3;
+
+public class Equation {
+    public static void main (String[] args) {
+        int a = 6;
+        int b = 25;
+        int y = 98;
+        int x = (y - b)*a;
+        System.out.println ("x = " + x);
+    }
+}
