8238cd13b382b7f19896f2ef1954c052741813de
AnastasiaBobkova <aubobkova@gmail.com>
2025-02-15 14:28:53 +0300

    Third task of home assignment 3

---
+++ src/homeassignments/task3/TernaryOperator.java
@@ -0,0 +1,11 @@
+package homeassignments.task3;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a = 35;
+        int b = 65;
+        int c = a > b ? 5 : 10;
+
+        System.out.println("c = " + c);
+    }
+}
