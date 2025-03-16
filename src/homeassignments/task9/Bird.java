1d48b339ca90d4778e39b0c51f1da80b2dbeb396
AnastasiaBobkova <aubobkova@gmail.com>
2025-03-16 15:36:24 +0300

    Home assignment 10

--- src/homeassignments/task9/Bird.java
+++ src/homeassignments/task9/Bird.java
@@ -14,11 +14,38 @@ public class Bird extends Animal{
         this.maxFlightAltitude = maxFlightAltitude;
     }
 
+    public String getKind() {
+        return kind;
+    }
+
+    public int getMaxFlightAltitude() {
+        return maxFlightAltitude;
+    }
+
     public String toString() {
         return("Птица -> "
                 + super.toString()
                 + ", Семейство: " + kind
                 + ", Максимальная высота полёта: " + maxFlightAltitude);
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Bird bird = (Bird) obj;
+        return  kind.equals(bird.getKind())
+                && maxFlightAltitude == bird.getMaxFlightAltitude()
+                && getColor().equals(bird.getColor())
+                && getMaxLifespan() == bird.getMaxLifespan()
+                && getFoodType().equals(bird.getFoodType());
+    }
+
+    public int hashCode () {
+        return getColor().hashCode() + getMaxLifespan() + getFoodType().hashCode() + kind.hashCode() + maxFlightAltitude*10;
     }
 
     public void makeSound() {
